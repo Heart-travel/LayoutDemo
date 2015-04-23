73960c294285f1a3b2f87dbcca9536120febe39a
Heart-travel <zcgacbc@gmail.com>
2015-04-23 13:42:05 +0800

    add a picture to a activity

--- src/com/example/layoutdemo/MainActivity.java
+++ src/com/example/layoutdemo/MainActivity.java
@@ -1,18 +1,39 @@
 package com.example.layoutdemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
+	private Button button;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		button = (Button) findViewById(R.id.button);
+		button.setOnClickListener(this);
 	}
 
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.button:
+				Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+				startActivity(intent);
+				break;
+			default:
+				break;
+		}
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
